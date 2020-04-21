23404ba2c4df95bff7168a94e91b601bfbdae88d
raptor_MVK <m.v.kamorin@gmail.com>
2020-04-21 21:18:45 +0300

    Add two servers

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 sudo -u www-data composer install -q
 sudo cp deploy/nginx.conf /etc/nginx/conf.d
+sudo sed -i -- "s|%SERVER_NAME%|$1|g" /etc/nginx/conf.d/nginx.conf
 sudo service nginx restart
 sudo -u www-data php bin/console doctrine:migrations:migrate --no-interaction
